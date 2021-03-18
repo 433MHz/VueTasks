f9f5a1a666bf2ee6de69914819600d3a9e961fba
Krystian Izdebski <krystian.izdebski1999@gmail.com>
2021-03-18 09:20:35 +0100

    All not implemented components added

---
+++ third/src/components/TB.vue
@@ -0,0 +1,36 @@
+<template>
+    <div>
+        <div><h3>TB convert</h3></div>
+        <div id="dataInput"><input type="number" v-model="MBValue"></div>
+        <Data :message="CalculatedValue"></Data>
+    </div>
+</template>
+
+<script>
+import Data from './Data'
+
+export default {
+
+    components:{
+        Data
+    },
+
+    data(){
+        return{
+            MBValue: Number,
+        }
+    },
+
+    computed:{
+        CalculatedValue(){
+            return this.MBValue * 1000000;
+        }
+    }
+}
+</script>
+
+<style>
+     #dataInput{
+        margin-bottom: 10px;
+    }
+</style>
